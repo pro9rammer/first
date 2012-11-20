9eef08b4629aa5da80991ef0332d3d15f08873a6
요한 김 <yohangim@yohan-gim-ui-MacBook-Pro.local>
2012-11-20 10:45:36 +0900

    Initial Commit

---
+++ ProjectResValidationTool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ProjectResValidationTool
+//
+//  Created by 요한 김 on 12. 11. 20..
+//  Copyright (c) 2012년 요한 김. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
